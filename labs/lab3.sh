15818cc44fab2cabe8af8d669da1bfc28c8c9fe0
George Melikov <mail@gmelikov.ru>
2024-11-07 22:48:40 +0300

    Don't recreate env on test

--- labs/lab3.sh
+++ labs/lab3.sh
@@ -142,9 +142,7 @@ case "$CMD" in
         ;;
     test)
         [ -n "$DEBUG" ] || check
-        create
         lab_test
-        delete
         ;;
     task)
         log "Need successfull ping $IP_NS1"
