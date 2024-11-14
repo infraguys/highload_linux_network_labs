e04b7aa8480db808071e31862d56d94a43666b42
George Melikov <mail@gmelikov.ru>
2024-11-14 12:59:49 +0300

    VMs: configure aliases + sync bash_history on every command

--- tools/terraform/main.tf
+++ tools/terraform/main.tf
@@ -50,6 +50,33 @@ git clone https://github.com/infraguys/highload_linux_network_labs.git
 chown debian:debian -R ./highload_linux_network_labs
 cd -
 
+echo "[TASK 6] Add some bash configurations"
+
+cat <<EOT | tee -a /home/debian/.bashrc /root/.bashrc
+
+# some more ls aliases
+alias ll='ls -alhF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# SYNC_HISTORY
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+HISTTIMEFORMAT="%Y.%m.%d %H:%M:%S "
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=20000
+HISTFILESIZE=20000
+
+# save each command to history file
+PROMPT_COMMAND='history -a'
+
+EOT
+
   EOF
 }
 
